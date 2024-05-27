6f5c543f0dab5b6eef43b475af86f66f7b7fecba
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-05-28 00:11:15 +0530

    change logout btn style

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -31,7 +31,7 @@ export default function Header() {
           {token && (
             <button
               type="button"
-              className="rounded-md bg-transparent px-3 py-2 text-sm font-semibold text-black hover:bg-black/10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+              className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
               onClick={handleLogout}
             >
               Log Out
